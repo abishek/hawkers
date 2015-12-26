08af42f412dc75b4eaf16ec6a105f071175a7370
Abishek Goda <goda.abishek@gmail.com>
2015-12-26 16:24:50 +0800

    initial add of app.js. app moved to angular. test data and basic page added.

---
+++ webui/js/app.js
@@ -0,0 +1,15 @@
+( function() {
+	var app = angular.module("hawkerApp", [ ]);
+	app.controller('HawkerAppController', ['$http', function($http) {
+		var ctrl = this;
+		ctrl.hawkers = [];
+		$http.get('js/hawker-data.json').success(function(data) {
+			ctrl.hawkers = data.hawkers;
+		});
+	}]);
+	app.controller('PinCodeController', ['$http', function($http) {
+		this.refine = function() {
+			console.log(this.pinCode);
+		};
+	}]);
+} )();
