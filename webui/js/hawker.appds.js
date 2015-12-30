58c19cd50c40b81da6e9ccb46ea53d316da76dcb
Abishek Goda <goda.abishek@gmail.com>
2015-12-30 12:04:37 +0800

    friendly messages, data sharing between controllers and some validation

---
+++ webui/js/hawker.appds.js
@@ -0,0 +1,25 @@
+( function() {
+angular.module("hawkerAppDS", []).factory('HawkerAppDS', function($rootScope) {
+	var service = {};
+	service.hawkerListMessage = "";
+	service.hawkerList = [];
+	service.orderList = [];
+
+	service.updateHawkerListMessage = function(message) {
+		this.hawkerListMessage = message;
+		$rootScope.$broadcast("messageUpdated");
+	}
+
+	service.updateHawkerList = function(hList) {
+		this.hawkerList = hList;
+		$rootScope.$broadcast("hawkerListUpdated");
+	}
+
+	service.updateOrderList = function(oList) {
+		this.orderList = oList;
+		$rootScope.$broadcast("orderListUpdated");
+	}
+
+	return service;
+});
+} )();
