bd59bac7782aaae4893c5e4a46e6f66342423662
Abishek Goda <goda.abishek@gmail.com>
2016-02-09 12:30:07 +0800

    database description and tables

--- flask/manage.py
+++ flask/manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from hawkers import app
-from flaskext.script import Manager, Shell, Server
+from flask.ext.script import Manager, Shell, Server
 
 manager = Manager(app)
 manager.add_command("runserver", Server())
@@ -9,9 +9,11 @@ manager.add_command("shell", Shell())
 
 @manager.command
 def createdb():
-    from hawker.models import db
+    from hawkers.models import db
     db.create_all()
 
-
+@manager.command
+def populatedb() :
+	from hawkers.models import db
+	
 manager.run()
-
