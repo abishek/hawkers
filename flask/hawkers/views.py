8f715875999bdc200c8fdd0d7c165072d3b94b8e
Abishek Goda <goda.abishek@gmail.com>
2016-02-26 08:48:21 +0800

    basic SQL fetch of hawkers by pincode they serve.

--- flask/hawkers/views.py
+++ flask/hawkers/views.py
@@ -82,11 +82,30 @@ def distance(pincode) :
 			
 @app.route('/list/<int:pincode>')
 def get_hawkers_by_pincode(pincode) :
-	# Loop through all the hawkers in your list and see who all would serve this pincode
-	if pincode == 111111 :
-		return jsonify(test_data)
-	else :
-		return jsonify({'hawkers':[]})
+	hawker_list = []
+	# See if this pincode exists in the cache. If yes, get hawker list from there
+	pc_entries = PincodeCache.query.filter_by(pincode=pincode)
+	for pc_entry in pc_entries :
+		hawker_list.append(Hawker.query.get(pc_entry.hawker_id))
+	
+	# Loop through all the hawkers in your list and see who all would serve this pincode		
+	hawkers = Hawker.query.all()
+	for hawker in hawkers :
+		# don't re-query known folks
+		if hawker in hawker_list :
+			continue
+		start_location = '%d, Singapore'%hawker.pincode
+		dest_location = '%d, Singapore'%pincode
+		dist = get_distance(gmaps, start_location, dest_location)
+		if dist <= 1000 and dist > 0 :
+			hawker_list.append(hawker)
+			# add hawker to cache
+			pc = PincodeCache()
+			pc.pincode = pincode
+			pc.hawker_id = hawker.id
+			db.session.add(pc)
+			db.session.commit()
+	return jsonify({'hawkers': hawker_list})
 
 @app.route('/order/place', methods=['POST'])
 def place_order() :
